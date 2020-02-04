5c5e5e7b6630423551437d6addc46cd8b8c6d712
ARCHITHA28 <59189028+ARCHITHA28@users.noreply.github.com>
2020-02-04 10:42:22 +0530

    Add files via upload

---
+++ datastructure.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int ch;
+do
+{
+printf("\n 1.push");
+printf("\n 2.pop");
+printf("\n 3.display");
+printf("\n 4.peek");
+printf("\n 5.exit");
+printf("\n enter your choice");
+scanf("%d",&ch);
+}
+while(ch!=5);
+return 0;
+}
